86b2557e5f93345260ada5f528e6e2377ffc26ef
patricia <patricia.poveda-boto@capgemini.com>
2021-12-07 08:36:05 +0100

    initial commit

---
+++ src/main/java/com/springboot/apirest/Practica712Application.java
@@ -0,0 +1,13 @@
+package com.springboot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica712Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica712Application.class, args);
+	}
+
+}
